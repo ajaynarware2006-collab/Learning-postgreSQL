9b8629b404e9fb3bfbf892ee90c4ebde8944bc37
ajaynarware2006-collab <ajaynarware2006@gmail.com>
2026-06-29 18:18:50 +0530

    exception handaling on banking system

--- banking_system.py
+++ banking_system.py
@@ -11,29 +11,58 @@ connection=pg.connect(
 
 cursor=connection.cursor()
 
+cursor.execute("SELECT * FROM account ORDER BY account_id")
+data=cursor.fetchall()
+
+def view_users():
+    for account in data:
+        print("=============================")
+        print(f"Name       : {account[1]}")
+        print(f"Amount     : {account[2]}")
+        print(f"Account ID : {account[0]}")
+        print("=============================")
+
+def check_user(account_id):
+    exists=False
+    for user in data:
+        if user[0]==account_id:
+            exists==True
+            return
+
+    if not exists:
+        print()
+        raise Exception("User not Exists")
+        
+def check_balance(amount,account_id):
+    cursor.execute("SELECT * FROM account WHERE account_id=%s",(account_id,))
+    sender=cursor.fetchone()
+    if sender[2]<amount:
+        raise Exception("Inefficient Balance")
+
+
+
 try:
     sender=int(input("Enter Sender's account ID : "))
+    check_user(sender)
+
     amount=int(input("Enter amount to send : "))
+    check_balance(amount,sender)
+
     reciver=int(input("Enter reciver's account ID : "))
+    check_user(reciver)
     print()
-    cursor.execute("UPDATE account SET amount=amount-%s WHERE account_id=%s",(amount,sender))
 
-    cursor.execute("UPDATE account SET amount=amount+%s WHERE account_id=%s",(amount,reciver))
+    if sender==reciver:
+        raise Exception("Sender and Reciver cannot be same")
+    
+    else:   
+        cursor.execute("UPDATE account SET amount=amount-%s WHERE account_id=%s",(amount,sender))
 
-    connection.commit()
-    print("Money Transfer seccessfully")
+        cursor.execute("UPDATE account SET amount=amount+%s WHERE account_id=%s",(amount,reciver))
+
+        connection.commit()
+        print("Money Transfer seccessfully")
 
 except Exception as e:
     connection.rollback()
     print(e)
-
-
-cursor.execute("SELECT * FROM account ORDER BY account_id")
-data=cursor.fetchall()
-for account in data:
-    print("=============================")
-    print(f"Name       : {account[1]}")
-    print(f"Amount     : {account[2]}")
-    print(f"Account ID : {account[0]}")
-    print("=============================")
-
